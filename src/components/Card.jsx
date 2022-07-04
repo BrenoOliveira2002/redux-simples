ed9ae5ad25125d407130edc7628c2d2c578662a5
BrenoOliveira2002 <breno_2002_@outlook.com>
2022-07-04 09:30:58 -0300

    card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,34 @@
 import './Card.css';
 import React from 'react';
 
+function getColor(props) {
+    
+    if(props.red) return "Red"
+    if(props.green) return "Green"
+    if(props.blue) return "Blue"
+    if(props.purple) return "Purple"
+    return ""
+
+}
+
 export default props => {
-    return( 
+    return (
 
-        <div>
+        <div className={`Card ${getColor(props)}`}>
+            <div className="Header">
 
-            Card
-            
+                <span className="Title"> {props.title}
+
+                </span>
+
+            </div>
+
+            <div>
+                <div className="Content"></div>
+                {props.children}
+
+            </div>
         </div>
 
-)
-    }
+    )
+}
